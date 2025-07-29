cfd295c271cd8003e842e4b95887a61291b430d0
Marek Stipek <mario.dweller@seznam.cz>
2025-07-29 13:39:01 +0200

    SlevomatCodingStandard.Classes.TraitUseSpacing: Not treating use as last in class when there are comments before the class closing brace

--- SlevomatCodingStandard/Sniffs/Classes/TraitUseSpacingSniff.php
+++ SlevomatCodingStandard/Sniffs/Classes/TraitUseSpacingSniff.php
@@ -154,7 +154,11 @@ class TraitUseSpacingSniff implements Sniff
 			$lastUseEndPointer = $tokens[$lastUseEndPointer]['bracket_closer'];
 		}
 
-		$pointerAfterLastUse = TokenHelper::findNextEffective($phpcsFile, $lastUseEndPointer + 1);
+		$firstTokenOnLineAfterLastUse = TokenHelper::findFirstTokenOnNextLine($phpcsFile, $lastUseEndPointer);
+		$pointerAfterLastUse = TokenHelper::findNextEffective($phpcsFile, $lastUseEndPointer + 1, $firstTokenOnLineAfterLastUse);
+
+		$pointerAfterLastUse ??= TokenHelper::findNextNonWhitespace($phpcsFile, $firstTokenOnLineAfterLastUse);
+
 		$isAtTheEndOfClass = $tokens[$pointerAfterLastUse]['code'] === T_CLOSE_CURLY_BRACKET;
 
 		$whitespaceEnd = TokenHelper::findNextNonWhitespace($phpcsFile, $lastUseEndPointer + 1) - 1;
---
+++ tests/Sniffs/Classes/data/traitUseSpacingModifiedSettingsWithCommentsAfterLastUseInClassErrors.fixed.php
@@ -0,0 +1,8 @@
+<?php
+
+class Whatever
+{
+	use A;
+
+	// comment on a new line
+}
